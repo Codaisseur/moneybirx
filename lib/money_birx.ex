80b2a6fe090bab84cdb571a8a3d373b051f99789
Wouter de Vos <wouter@codaisseur.com>
2019-10-10 10:40:40 +0200

    Init

---
+++ lib/money_birx.ex
@@ -0,0 +1,18 @@
+defmodule MoneyBirx do
+  @moduledoc """
+  Documentation for MoneyBirx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MoneyBirx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/money_birx_test.exs
@@ -0,0 +1,8 @@
+defmodule MoneyBirxTest do
+  use ExUnit.Case
+  doctest MoneyBirx
+
+  test "greets the world" do
+    assert MoneyBirx.hello() == :world
+  end
+end
