ab6a4efa166e378537f8e03802f4d9f49fd7457a
Wouter de Vos <wouter@codaisseur.com>
2019-10-16 13:41:26 +0200

    Update root module documentation

--- lib/moneybirx.ex
+++ lib/moneybirx.ex
@@ -1,18 +1,26 @@
 defmodule Moneybirx do
   @moduledoc """
-  Documentation for Moneybirx.
+  This is an unofficial [Moneybird API](https://developer.moneybird.com/) client. It uses HTTPoison for requests and Poison for dealing with JSON payloads.
+
+  ## Installation
+
+  The package can be installed by adding `moneybirx` to your list of dependencies in `mix.exs`:
+
+      def deps do
+        [
+          {:moneybirx, "~> 0.1.0"}
+        ]
+      end
+
+  ## Configuration
+
+  First go to https://moneybird.com/user/applications/new and create an API token for **Personal Use**. Then configure Moneybirx to use your token:
+
+      # config/config.exs
+
+      config :moneybirx,
+        endpoint: "https://moneybird.com/api/v2",
+        token: "your-api-token"
+
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Moneybirx.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
