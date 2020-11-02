acf552e1b912c77a919a5563d37c51e0db67032b
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2020-11-02 19:12:58 +0100

    Bump to v0.1.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Moneybirx.MixProject do
   def project do
     [
       app: :moneybirx,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
